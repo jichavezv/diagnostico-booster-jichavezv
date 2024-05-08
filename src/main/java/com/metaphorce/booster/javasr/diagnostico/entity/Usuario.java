db6aad51fb8d2441216d95c41fb4e9d64de7299e
jichavezv <chavezvelajuanignacio@gmail.com>
2024-05-08 12:32:40 -0600

    [FEAT] Adding docker an docke-compose

--- src/main/java/com/metaphorce/booster/javasr/diagnostico/entity/Usuario.java
+++ src/main/java/com/metaphorce/booster/javasr/diagnostico/entity/Usuario.java
@@ -21,6 +21,14 @@ public class Usuario {
 	
 	private String rol;
 	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
 	public String getNombreUsuario() {
 		return nombreUsuario;
 	}
